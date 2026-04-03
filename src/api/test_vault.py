206e766cfc128285ee32a42f5bb8886ed2d70fac
BorisVikVik <boris@victorov.com>
2026-04-03 19:57:10 +0300

    After ubnderstnding bug with db

--- src/api/test_vault.py
+++ src/api/test_vault.py
@@ -3,12 +3,10 @@ import sys
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # Загружает .env в os.environ
+load_dotenv() 
 
-# DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_PSW = os.getenv('DB_PSW')
 
-# print(VAULT_TOKEN)  # Значение из .env
 client = hvac.Client(
     url='http://vault:8200',
     token='testtoken',
@@ -21,14 +19,3 @@ create_response = client.secrets.kv.v2.create_or_update_secret(
 )
 
 print('Secret written successfully.')
-
-# # read_response = client.secrets.kv.read_secret_version(path='my-secret-password')
-
-# # password = read_response['data']['data']['password']
-
-
-
-# # if password != 'test123':
-# #     sys.exit('unexpected password')
-
-# # print('Access granted!')
